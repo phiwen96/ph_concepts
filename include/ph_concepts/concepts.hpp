6bc9bc59cc7200755f1b9b44ddeffec598bfb2e4
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-03-28 16:23:51 +0200

    a

--- include/ph_concepts/concepts.hpp
+++ include/ph_concepts/concepts.hpp
@@ -7,11 +7,32 @@ using namespace experimental;
 namespace ph::concepts{
 
 template <class T, class U>
-concept convertible = std::is_convertible_v <T, U>;
+concept convertible = requires (T t, U u) {//std::is_convertible_v <T, U>;
+    {t = u};
+};
 
 template <class T, class U>
 concept same = std::is_same_v <T, U>;
 
+template <class T>
+concept copy_constructible = requires (T const& t) {
+    T (t);
+};
+
+template <class T>
+concept move_constructible = requires (T&& t) {
+    T ((T&&) t);
+};
+
+template <class T>
+concept copy_assignable = requires (T& t1, T const& t2) {
+    t1 = t2;
+};
+
+template <class T>
+concept move_assignable = requires (T& t1, T&& t2) {
+    t1 = (T&&) t2;
+};
 
 
 
@@ -20,14 +41,16 @@ concept same = std::is_same_v <T, U>;
 
 
 template <typename T>
-concept coroutine = requires (typename T::promise_type promise) {
+concept coroutine = move_constructible <T> and not copy_constructible <T> and requires (typename T::promise_type promise) {
 
     {promise.get_return_object()} -> convertible<T>;
     promise.initial_suspend();
     {promise.final_suspend()} noexcept;
-    {promise.return_void()} -> same <void>;
+//    {promise.return_void()} -> same <void>;
     {promise.unhandled_exception()} -> same <void>;
 };
+
+
 
 template <typename T>
 concept awaitable = requires (T const t) {
@@ -45,9 +68,25 @@ concept fast_awaitable = requires (T const t) {
 };
 
 
+template <class T>
+concept is_lazy_coroutine = coroutine <T> and requires (typename T::promise_type p) {
+    {p.initial_suspend ()} -> convertible <suspend_always>;
+    
+    /**
+     user of the coroutine gets
+     a chance to read the value
+     when "yield 6"
+     */
+    {p.yield_value (declval <typename T::value_type> ())} -> convertible <suspend_always>;
+};
 
 
 
+static_assert(same <int, double> == false, "");
+static_assert(same <int, int> == true, "");
+static_assert(convertible <int, double> == true, "");
+static_assert(convertible <double, int> == true, "");
+static_assert(convertible <int, string> == false, "");
 
 
 
