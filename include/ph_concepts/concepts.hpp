5b924d95b0d800a71564ad44fdb8f57add1edb2a
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-06-03 13:31:16 +0200

    a

--- include/ph_concepts/concepts.hpp
+++ include/ph_concepts/concepts.hpp
@@ -1,3 +1,92 @@
 #pragma once
 //using namespace std;
 
+
+template <typename T, typename... U>
+concept same_as_any_of = (std::is_same_v <T, U> or ...);
+
+
+
+
+template <char c>
+concept is_digit = requires {requires (c >= '0' and c <= '9');};
+
+template <typename A, typename B>
+concept same_as = requires (){
+    requires std::is_same_v <A, B>;
+};
+
+
+template <typename A, typename B>
+concept convertible_to = requires (){
+    requires std::is_convertible_v <A, B>;
+};
+
+template <typename T>
+concept Pointer = requires (T& t)
+{
+    requires (std::is_pointer_v <T>);
+};
+
+template <typename T>
+concept Iterator = requires (T& t)
+{
+    {++t} -> same_as <T&>;
+    {t++} -> same_as <T&>;
+    {--t} -> same_as <T&>;
+    {t--} -> same_as <T&>;
+    
+    requires requires ()
+    {
+        {*t} -> same_as <typename T::reference>;
+        
+    } or requires ()
+    {
+        {*t} -> same_as <typename T::value_type>;
+    };
+};
+
+
+
+template <typename T>
+concept Iterable = requires (T& t)
+{
+    requires requires ()
+    {
+        {t.begin ()} -> Iterator;
+        {t.end ()} -> Iterator;
+        
+    } or requires ()
+    {
+        {t.begin ()} -> Pointer;
+        {t.end ()} -> Pointer;
+        
+    } or requires (size_t& i)
+    {
+        t [i];
+        {t.size ()} -> convertible_to <size_t>;
+    };
+};
+
+
+
+
+template <typename T>
+concept String = requires (T& str)
+{    
+    requires (std::is_same_v <typename T::value_type, char>);
+    
+    requires requires (size_t& i)
+    {
+        {str [i]} -> same_as <char&>;
+    };
+    
+    {str.size ()} -> convertible_to <size_t>;
+    
+    
+    
+};
+
+
+
+
--- src/concepts.cpp
+++ src/concepts.cpp
@@ -1,7 +1,43 @@
 #include "concepts.hpp"
 
+static_assert (same_as_any_of <int, double, float, int>, "");
+static_assert (not same_as_any_of <int, double, float, std::string>, "");
+//static_assert (Iterable <std::vector <int>>, "");
+
+
+struct iter
+{
+    using reference = int&;
+    using value_type = int;
+    int i;
+    int* begin(){return &i;}
+    int* end(){return &i;}
+    auto& operator++(){return *this;}
+    auto& operator++(int){return *this;}
+    auto& operator--(){return *this;}
+    auto& operator--(int){return *this;}
+    auto operator*(){return i;}
+};
+
+
+
+static_assert (Iterator <iter>, "");
 
 
 
 
 
+
+struct string
+{
+    using value_type = char;
+    char c;
+    auto& operator[](int) {return c;}
+    auto size () const{return 2;}
+    
+};
+
+
+static_assert (String <string>, "");
+static_assert (String <std::string>, "");
+static_assert (String <std::vector <char>>, "");
