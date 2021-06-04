8589f5edcd575bb3922e48dd988fa7f8c90bc427
Philip Wenkel <38218064+phiwen96@users.noreply.github.com>
2021-06-04 12:55:13 +0200

    a

---
+++ tests/src/test_concepts.cpp
@@ -0,0 +1,23 @@
+//#include "testlib.hpp"
+
+#ifdef RUN_CATCH
+
+//#include "test.hpp"
+#include <catch2/catch.hpp>
+#include <philip/philip.hpp>
+#include <ph_file/file.hpp>
+
+
+
+using namespace std;
+
+TEST_CASE ("app")
+{
+    char* source = ph::file::read (TEST_FILE);
+    free (source);
+}
+
+
+
+
+#endif
